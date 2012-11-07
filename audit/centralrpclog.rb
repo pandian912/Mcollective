ac4d7e889daad0f42c2f761168a10bb40ad87d08
jp <jp@jayapandian.com>
2012-11-07 15:15:55 +0530

    added the modified centralrpclog

--- audit/centralrpclog.rb
+++ audit/centralrpclog.rb
@@ -8,26 +8,31 @@ module MCollective
       def audit_request(request, connection)
         begin
           config = Config.instance
-
+          log_collective = Config.instance.pluginconf["centralrpclog.collective"] || config.main_collective
+          filter = {"agent" => "centralrpclog"}
+          req = Message.new(request, nil, {:agent => "centralrpclog", :type => :request, :collective => log_collective, :filter => filter})
+          req.encode!
           # First try the new sub collectives method of obtaining the required collective, if that fails
           # because make_target doesnt take a collective fall back to old behavior
-          begin
-            log_collective = Config.instance.pluginconf["centralrpclog.collective"] || config.main_collective
-            target = Util.make_target("centralrpclog", :command, log_collective)
-          rescue
-            target = Util.make_target("centralrpclog", :command)
-          end
+          ##begin
+          ##  log_collective = Config.instance.pluginconf["centralrpclog.collective"] || config.main_collective
+          ##  target = Util.make_target("centralrpclog", :command, log_collective)
+          ##rescue
+          ##  target = Util.make_target("centralrpclog", :command)
+          ##end
 
-          reqid = Digest::MD5.hexdigest("#{config.identity}-#{Time.now.to_f.to_s}-#{target}")
-          filter = {"agent" => "centralrpclog"}
+          ##reqid = Digest::MD5.hexdigest("#{config.identity}-#{Time.now.to_f.to_s}-#{target}")
+          ##filter = {"agent" => "centralrpclog"}
 
-          req = PluginManager["security_plugin"].encoderequest(config.identity, target, request, reqid, filter)
+          ##req = PluginManager["security_plugin"].encoderequest(config.identity, target, request, reqid, filter)
 
-          if connection.respond_to?(:publish)
-            connection.publish(target, req)
-          else
-            connection.send(target, req)
-          end
+          ##if connection.respond_to?(:publish)
+          ##  connection.publish(target, req)
+          ##else
+          ##  connection.send(target, req)
+          ##end
+          req.publish
+          Log.debug("Sending request #{req.requestid} to the #{req.agent} agent in collective #{req.collective}")
         rescue Exception => e
           Log.instance.error("Failed to send audit request: #{e}")
         end
